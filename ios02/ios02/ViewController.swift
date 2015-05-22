7530759cb7e54b34690039e067d7238faede97b6
xudingjiemike <xudingjie_mike@163.com>
2015-05-22 14:30:27 +0800

    ios

---
+++ ios02/ios02/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ios02
+//
+//  Created by apple on 15/1/3.
+//  Copyright (c) 2015年 apple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
