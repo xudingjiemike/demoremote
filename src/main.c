79cc8c800ba3b68dc30073c5dd5c7cd7a65d6722
xudingjiemike <xudingjie_mike@163.com>
2015-05-25 17:14:36 +0800

    merge third

---
+++ src/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  main
+//
+//  Created by apple on 15/5/22.
+//  Copyright (c) 2015年 apple. All rights reserved.
+//
+#include "version.h"
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    printf("Hello,world.\n");
+    printf("version:%s.\n",_VERSION);
+    return 0;
+}
